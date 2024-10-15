800438117737ccb450d2059e4b09e57fa07bd38e
Dauarim <greencode.kz@gmail.com>
2024-10-15 16:17:03 +0500

    add hover effekt for post

--- components/post/post.jsx
+++ components/post/post.jsx
@@ -1,9 +1,18 @@
+import Link from 'next/link'
 import './post.css'
 
 export default function post({post}) {
     return(
         <div className="post">
-            <img src={post.image} alt="" />
+            <Link href=''> 
+                {/* <img src={post.image} alt="" />       */}
+            </Link>
+            <div>
+                <img src="/images/icons/notifications.png" alt="" />
+                <span>{post.like}</span>
+                <img src="/images/icons/comments.png" alt="" />
+                <span>{post.comments}</span>
+            </div>
         </div>
     )
 }
--- components/posts-list/posts.jsx
+++ components/posts-list/posts.jsx
@@ -5,7 +5,10 @@ export default function posts() {
     return(
         <div className="posts">
             <div className="post-list-header">
-
+                <div className='active'>
+                    <a href="">публикации</a>
+                </div> 
+                <div><a href="">отметки</a></div>
             </div>
             <div className="post-list">
                 {POSTSDB.map(post => 
